5f72fb08774bc819ced57e6e1fca238cc31fb716
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-04-24 15:16:35 +0100

    WIP

---
+++ DFC.Digital.Tools/DFC.Digital.Tools.Data/Interfaces/IApplicationLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DFC.Digital.Tools.Data.Interfaces
+{
+    public interface IApplicationLogger
+    {
+        void Trace(string message);
+
+        void Info(string message);
+
+        void Warn(string message, Exception ex);
+
+        void Warn(string message);
+
+        void Error(string message, Exception ex);
+
+        void ErrorJustLogIt(string message, Exception ex);
+
+        string LogExceptionWithActivityId(string message, Exception ex);
+
+        bool IsTraceDisabled();
+    }
+}
--- DFC.Digital.Tools/DFC.Digital.Tools.Service.GovUkNotify/GovUkNotifyService.cs
+++ DFC.Digital.Tools/DFC.Digital.Tools.Service.GovUkNotify/GovUkNotifyService.cs
@@ -1,14 +1,55 @@
 ﻿using DFC.Digital.Tools.Data.Interfaces;
 using DFC.Digital.Tools.Data.Models;
-using System;
+using Notify.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DFC.Digital.Tools.Service.GovUkNotify
 {
     public class GovUkNotifyService : ISendCitizenNotification<CitizenEmailNotification>
     {
+        private readonly IApplicationLogger applicationLogger;
+        private readonly IGovUkNotifyClientProxy clientProxy;
+
+        public GovUkNotifyService(IApplicationLogger applicationLogger, IGovUkNotifyClientProxy clientProxy)
+        {
+            this.applicationLogger = applicationLogger;
+            this.clientProxy = clientProxy;
+        }
+
         public bool SendCitizenNotification(CitizenEmailNotification notification)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var response = clientProxy.SendEmail("apikey", notification.EmailAddress, "templateId", this.Convert(notification.EmailPersonalisation));
+                return !string.IsNullOrEmpty(response?.id);
+            }
+            catch (NotifyClientException ex)
+            {
+                applicationLogger.ErrorJustLogIt("Failed to send VOC email", ex);
+                return false;
+            }
+        }
+
+        public Dictionary<string, dynamic> Convert(GovUkNotifyPersonalisation vocSurveyPersonalisation)
+        {
+            if (vocSurveyPersonalisation?.Personalisation != null)
+            {
+                foreach (var item in vocSurveyPersonalisation?.Personalisation?.ToArray())
+                {
+                    if (string.IsNullOrEmpty(item.Value) && vocSurveyPersonalisation != null)
+                    {
+                        vocSurveyPersonalisation.Personalisation[item.Key] = "uknown";
+                    }
+                }
+
+                return vocSurveyPersonalisation?.Personalisation
+                    .ToDictionary<KeyValuePair<string, string>, string, dynamic>(
+                        vocObj => vocObj.Key,
+                        vocObj => vocObj.Value);
+            }
+
+            return null;
         }
     }
 }
