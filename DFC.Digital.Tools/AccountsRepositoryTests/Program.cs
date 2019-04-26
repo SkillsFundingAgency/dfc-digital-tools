93e34cae63c59a1ebf73920bdc90cf86c4b1c48a
gurney <Gurney@as-tech.co.uk>
2019-04-26 14:03:40 +0100

    Added accounts service stub

---
+++ DFC.Digital.Tools/AccountsRepositoryTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccountsRepositoryTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+
+            Console.WriteLine("Ending");
+            Console.ReadKey();
+        }
+    }
+}
