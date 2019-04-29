6672a2d1782a8bb74097ccb2163d5c87cd12f6fd
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-04-29 23:34:22 +0100

    finished unit testing the govuknotify service library

--- DFC.Digital.Tools/DFC.Digital.Tools.Service.GovUkNotify/Config/GovUkNotifyAutofacModule.cs
+++ DFC.Digital.Tools/DFC.Digital.Tools.Service.GovUkNotify/Config/GovUkNotifyAutofacModule.cs
@@ -1,7 +1,9 @@
 ﻿using Autofac;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.Digital.Tools.Service.GovUkNotify
 {
+    [ExcludeFromCodeCoverage]
     public class GovUkNotifyAutofacModule : Module
     {
         protected override void Load(ContainerBuilder builder)
