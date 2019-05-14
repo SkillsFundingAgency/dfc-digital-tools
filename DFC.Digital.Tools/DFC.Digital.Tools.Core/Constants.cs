cdeb1c6f8b04593e5fafe914e054c5b29c018acc
gurney <Gurney@as-tech.co.uk>
2019-05-14 10:43:47 +0100

    Removed no longer used constants

--- DFC.Digital.Tools/DFC.Digital.Tools.Core/Constants.cs
+++ DFC.Digital.Tools/DFC.Digital.Tools.Core/Constants.cs
@@ -3,13 +3,11 @@
     public static class Constants
     {
         public const string ApplicationInsightsInstrumentationKey = "APPINSIGHTS_INSTRUMENTATIONKEY";
-        public const string GovUkNotifySection = "GovUkNotify";
         public const string GovUkNotifyApiKey = "DFC.Tools.GovUkNotifyAPIKey";
         public const string GovUkNotifyTemplateId = "DFC.Digital.Tools.GovUkNotifyTemplateId";
         public const string GovUkNotifyRetryCount = "DFC.Digital.Tools.GovUkNotifyRetryCount";
         public const string UnknownValue = "Unknown";
         public const string GovUkNotifyRateLimitException = "DFC.Digital.Tools.GovUkNotifyRateLimitException";
-        public const string AccountRepositorySection = "AccountRepository";
         public const string SQLConnection = "DFC.Digital.Tools.SQLConnection";
         public const string BatchSize = "DFC.Digital.Tools.BatchSize";
         public const string CutOffDate = "DFC.Digital.Tools.CutOffDate";
